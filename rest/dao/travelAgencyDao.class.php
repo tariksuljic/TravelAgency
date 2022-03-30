f2fe0fcc3a8f7898e3f2cee4dba87ddd2701d47d
Tarik <tarik905.suljic@hotmail.com>
2022-03-30 12:10:23 +0200

    changed connection variable values, implemented get_by_id method

--- rest/dao/travelAgencyDao.class.php
+++ rest/dao/travelAgencyDao.class.php
@@ -9,10 +9,10 @@ class travelAgencyDao{
 
   public function __construct(){
 
-    $servername="sql11.freemysqlhosting.net";
-    $username="sql11482437";
-    $password="WkH8WvvA9Q";
-    $schema="sql11482437";
+    $servername="127.0.0.1:3308";
+    $username="travelAgency";
+    $password="travelAgency123";
+    $schema="travelagency";
 
     $this->conn=new PDO("mysql:host=$servername;dbname=$schema",$username,$password);
     $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
@@ -26,21 +26,21 @@ class travelAgencyDao{
 
     public function get_all(){
 
-    $stmt=$this->conn->prepare("SELECT * FROM Users");
+    $stmt=$this->conn->prepare("SELECT * FROM users");
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
   }
 
   //Method used to add user to the database
   public function add($description,$created){
-    $stmt=$this->conn->prepare("INSERT INTO Users (description,created) VALUES(:description,:created)");
+    $stmt=$this->conn->prepare("INSERT INTO users (description,created) VALUES(:description,:created)");
     $stmt->execute(['description'=>$description,'created'=>$created]);
   }
 
   //Method to delete record from the database
   public function delete($id){
 
-    $stmt=$this->conn->prepare("DELETE FROM Users WHERE id=:id");
+    $stmt=$this->conn->prepare("DELETE FROM users WHERE id=:id");
     $stmt->bindParam(':id',$id);
     $stmt->execute();
 
@@ -50,11 +50,23 @@ class travelAgencyDao{
 
   public function update($id,$description,$created){
 
-    $stmt=$this->conn->prepare("UPDATE Users SET description=:description,created=:created WHERE id=:id");
+    $stmt=$this->conn->prepare("UPDATE users SET description=:description,created=:created WHERE id=:id");
     $stmt->bindParam(':id',$id);
     $stmt->bindParam(':description',$description);
     $stmt->bindParam(':created',$created);
     $stmt->execute();
+
+  }
+
+//Method to get user by id
+  public function get_by_id($id){
+
+    $stmt=$this->conn->prepare("SELECT * FROM users WHERE id=:id");
+    $stmt->bindParam(':id',$id);
+    $stmt->execute();
+    $result=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    return @reset($result);
+
 
   }
 
